133d8637ff85aa7958a9ff7a138fb4d78d537c77
Stefan Hilker <hilker@wave-solutions.de>
2025-08-21 09:01:22 +0200

    Add React markdown

--- packages/client/src/components/ChatBot.tsx
+++ packages/client/src/components/ChatBot.tsx
@@ -1,6 +1,7 @@
 import { Button } from './ui/button';
 import { FaArrowUp } from 'react-icons/fa';
 import { useForm } from 'react-hook-form';
+import ReactMarkdown from 'react-markdown';
 import axios from 'axios';
 import { useRef, useState, type KeyboardEvent } from 'react';
 
@@ -44,7 +45,7 @@ const ChatBot = () => {
                 : 'bg-gray-100 text-black self-start'
             }`}
           >
-            {message.content}
+            <ReactMarkdown>{message.content}</ReactMarkdown>
           </p>
         ))}
       </div>
