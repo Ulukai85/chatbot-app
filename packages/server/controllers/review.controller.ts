25b1448016ff33abc338647c174c5b6d2e9c5b46
Ulukai85 <ueffeldie7@gmx.de>
2025-08-21 20:30:48 +0200

    Handle edge cases

--- packages/server/controllers/review.controller.ts
+++ packages/server/controllers/review.controller.ts
@@ -1,5 +1,7 @@
 import type { Request, Response } from 'express';
 import { reviewService } from '../services/review.service';
+import { productRepository } from '../repositories/product.repository';
+import { reviewRepository } from '../repositories/review.repository';
 
 export const reviewController = {
   async getReviews(req: Request, res: Response) {
@@ -21,6 +23,18 @@ export const reviewController = {
       res.status(400).json({ error: 'Invalid product ID.' });
       return;
     }
+
+    const product = await productRepository.getProduct(productId);
+    if (!product) {
+      res.status(400).json({ error: 'Invalid product' });
+      return;
+    }
+
+    const reviews = await reviewRepository.getReviews(productId, 1);
+    if (!reviews.length) {
+      res.status(400).json({ error: 'There are no reviews to summarize.' });
+    }
+
     const summary = await reviewService.summarizeReviews(productId);
     res.json({ summary });
   },
