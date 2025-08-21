83c75b3c51e519a00ca22b61b3b1c4071010f061
Stefan Hilker <hilker@wave-solutions.de>
2025-08-21 07:27:26 +0200

    Add chatService and conversationRepository

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,14 +1,10 @@
 import express from 'express';
 import type { Request, Response } from 'express';
-import dotenv, { parse } from 'dotenv';
-import OpenAi from 'openai';
+import dotenv from 'dotenv';
 import z from 'zod';
+import { chatService } from './services/chat.service';
 
 dotenv.config();
-
-const client = new OpenAi({
-  apiKey: process.env.OPENAI_API_KEY,
-});
 
 const app = express();
 app.use(express.json());
@@ -21,8 +17,6 @@ app.get('/', (req: Request, res: Response) => {
 app.get('/api/hello', (req: Request, res: Response) => {
   res.json({ message: 'Hello world' });
 });
-
-const conversations = new Map<string, string>();
 
 const chatSchema = z.object({
   prompt: z
@@ -42,17 +36,9 @@ app.post('/api/chat', async (req: Request, res: Response) => {
 
   try {
     const { prompt, conversationId } = req.body;
-    const response = await client.responses.create({
-      model: 'gpt-4o-mini!',
-      input: prompt,
-      temperature: 0.2,
-      max_output_tokens: 100,
-      previous_response_id: conversations.get(conversationId),
-    });
+    const response = await chatService.sendMessage(prompt, conversationId);
 
-    conversations.set(conversationId, response.id);
-
-    res.json({ message: response.output_text });
+    res.json({ message: response.message });
   } catch (error) {
     res.status(500).json({ error: 'Failed to generate a response.' });
   }
